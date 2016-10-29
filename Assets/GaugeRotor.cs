4531c356098d3ef72e8a4cfa94ce66fd61971873
tdvance <tdvance@gmail.com>
2016-10-28 20:25:10 -0400

    Working on gauges

--- Assets/GaugeRotor.cs
+++ Assets/GaugeRotor.cs
@@ -3,18 +3,22 @@ using System.Collections;
 
 public class GaugeRotor : MonoBehaviour {
 
-    public float minValue = -50;
-    public float maxValue = 50;
+    public float minValue = 0;
+    public float maxValue = 100;
     public float gaugeValue = 0;
+    public float direction = -1f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        float angle = (gaugeValue - minValue) / (maxValue - minValue) * 60f  + 65f;
-        transform.eulerAngles = new Vector3(angle, 0, 0);
-	}
+    private Vector3 startRotation;
+
+    // Use this for initialization
+    void Start() {
+        startRotation = transform.eulerAngles;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        float angle = (gaugeValue - minValue) / (maxValue - minValue) * 100f;
+        transform.eulerAngles = startRotation + direction * new Vector3(angle, 0, 0);
+    }
 }
--- Assets/Thruster.cs
+++ Assets/Thruster.cs
@@ -11,28 +11,68 @@ public class Thruster : MonoBehaviour {
     public Vector3 velocity;
     Rigidbody rb;
 
+    private GaugeRotor leftRotor;
+    private GaugeRotor rightRotor;
+
     // Use this for initialization
     void Start() {
         rb = GetComponent<Rigidbody>();
+        GaugeRotor[] rotors = FindObjectsOfType<GaugeRotor>();
+        foreach (GaugeRotor rotor in rotors) {
+            if (rotor.name.ToLower().StartsWith("left")) {
+                leftRotor = rotor;
+            }else {
+                rightRotor = rotor;
+            }
+        }
+        rightRotor.maxValue = fuel;
+        rightRotor.gaugeValue = fuel;
     }
 
     // Update is called once per frame
     void FixedUpdate() {
         if (CrossPlatformInputManager.GetButton("Jump") || CrossPlatformInputManager.GetButton("Fire1")) {
             thrust(0.6f);
+        }else {
+            if (leftRotor.gaugeValue > 0) {
+                leftRotor.gaugeValue -= 1f;
+                if (leftRotor.gaugeValue < 0) {
+                    leftRotor.gaugeValue = 0;
+                }
+            }
         }
+        rightRotor.gaugeValue = fuel;
         velocity = rb.velocity;
     }
 
     void thrust(float fraction) {
         if (fraction > 0 && fuel > 0) {
             Vector3 force = transform.up * fraction * maxThrust;
-            float dmds = (maxThrust * fraction) / Physics.gravity.magnitude/16f / specificImpulse;
+            float dmds = (maxThrust * fraction) / Physics.gravity.magnitude / 16f / specificImpulse;
             print("fuel used = " + dmds);
             fuel -= dmds;
             if (fuel > 0) {
                 rb.velocity += force / (rb.mass + fuel) * Time.fixedDeltaTime;
             }
+            if (leftRotor.gaugeValue > fraction * 100) {
+                leftRotor.gaugeValue -= 1f;
+                if (leftRotor.gaugeValue < fraction * 100) {
+                    leftRotor.gaugeValue = fraction * 100;
+                }
+            } else if (leftRotor.gaugeValue < fraction * 100) {
+                leftRotor.gaugeValue += 1f;
+                if (leftRotor.gaugeValue > fraction * 100) {
+                    leftRotor.gaugeValue = fraction * 100;
+                }
+            }
+        } else {
+            if (leftRotor.gaugeValue > 0) {
+                leftRotor.gaugeValue -= 1f;
+                if (leftRotor.gaugeValue < 0) {
+                    leftRotor.gaugeValue = 0;
+                }
+            }
         }
+
     }
 }
